6704aefdec3fff1778fb8b40310eb2829a292109
Ian Dietsch <wolffox36007@gmail.com>
2020-02-11 16:30:43 -0500

    Add project files.

---
+++ FirstGame/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FirstGame {
+    class Program {
+        static void Main(string[] args) {
+
+            PigDiceGame pigDice = new PigDiceGame();
+            pigDice.Play();
+        }
+    }
+}
